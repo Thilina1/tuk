f04919cb79f873ca497a5469e692480c79d5a9b3
thilinaweerasing <thilinaweerasing@gmail.com>
2025-09-27 13:11:37 +0000

    fix

--- src/app/api/payhere/checkout/api/route.js
+++ src/app/api/payhere/checkout/api/route.js
@@ -7,7 +7,8 @@ export async function POST(req) {
   const data = await req.json();
   const { orderId, amount, currency } = data;
 
-  const merchant_id = "1231320";
+
+  const merchant_id = "1231320"; 
   const merchant_secret = "MzEyMTIwMjE5OTY0NjQzNDM4MzE5MTc1ODM4MDIxMDAyNjIwODc=";
 
   
