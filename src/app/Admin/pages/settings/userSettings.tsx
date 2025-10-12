b23cd3a59e1b02ce59e6ec68e73eb890af0e6584
thilinaweerasing <thilinaweerasing@gmail.com>
2025-10-12 15:11:45 +0000

    fix

--- src/app/Admin/pages/settings/userSettings.tsx
+++ src/app/Admin/pages/settings/userSettings.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
 import { db } from "@/config/firebase";
-import { FaUserPlus, FaEdit, FaTrash, FaEyeSlash } from "react-icons/fa";
+import { FaUserPlus, FaEdit, FaTrash } from "react-icons/fa";
 
 type AdminUser = {
   id: string;
